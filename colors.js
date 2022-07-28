1619eccb590f0f26f82942a46812f190a01db08f
LYM <72262985+turegold@users.noreply.github.com>
2022-07-28 22:17:59 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,18 +1,21 @@
 var Body = {
     setColor:function(color){
-        document.querySelector('body').style.color = color;
+        //document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackGroundColor:function(color){
-        document.querySelector('body').style.backgroundColor = color;
+        //document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }  
 }
 
 var Links={
     setColor:function(color){
-        var alist = document.querySelectorAll('a');
-        for(var i;i < alist.length;i++){
-            alist[i].style.color = 'white';
-        }
+        //  var alist = document.querySelectorAll('a');
+        //  for(var i;i < alist.length;i++){
+        //      alist[i].style.color = 'white';
+        //  }
+    $('a').css('color', color);
     }
 }
 
